90a5b6d79eb2f0d24a88267dc55209390b430cee
Carlos Paniagua <cpaniaguam@gmail.com>
2022-10-18 15:16:56 -0400

    feat: spetialized eos_g_rho_g

--- src/initial.jl
+++ src/initial.jl
@@ -19,6 +19,16 @@ function eos_g(P::Number, T::Number)
 end
 
 """
+    eos_g_rho_g(P::Number, T::Number)::Float64
+
+Spetialized version of eos_g that computes `rho_g` only.
+"""
+function eos_g_rho_g(P::Number, T::Number)::Float64
+    ρ = -112.528*Complex(T-273.15)^-0.381 + 127.811*Complex(P*1e-5)^-1.135 + 112.04*Complex(T-273.15)^-0.411*Complex(P*1e-5)^0.033
+    return real(ρ*1e3)
+end
+
+"""
     exsolve_silicic(P::Number, T::Number, X_co2::Number)
 
 This script uses Liu et al. (2006) to calculate the solubility of water
@@ -389,32 +399,37 @@ function parameters_melting_curve_mafic(mH2O::Number, mCO2::Number, P::Number)
 end
 
 """
-    find_liq(composition::String, water::Number, co2::Number, P::Number, ini_eps_x::Number)
+    find_liq_silicic(water::Number, co2::Number, P::Number, ini_eps_x::Number)
 
 # Arguments
--`composition`: "silicic" or "mafic"
 -`water`: Weight fration of the H2O in magma.
 -`co2`: Weight fration of the CO2 in magma.
 -`P`: Pressure (Pa)
 -`ini_eps_x`: The starting volumn fraction of crystal.
 """
-function find_liq(composition::String, water::Number, co2::Number, P::Number, ini_eps_x::Number)
-    if !(composition in ["silicic", "mafic"])
-        error("composition must be \"silicic\" or \"mafic\".")
-    else
-        if composition == "silicic"
-            a, dadx, dady, dadz, b, dbdx, dbdy, dbdz, c, dcdx, dcdy, dcdz = parameters_melting_curve_silicic(100*water,100*co2,P)
-            f(x) = a*erfc(b*(x-c))-ini_eps_x
-            x0 = 1000
-            Tll = fzero(f, (0, x0), maxevals=100)
-            Tl = Tll + 273.15
-        elseif composition == "mafic"
-            a, dadx, dady, dadz, b, dbdx, dbdy, dbdz = parameters_melting_curve_mafic(100*water,100*co2,P)
-            Tl = (ini_eps_x-b)/a
-            Tl = Tl + 273.15
-        end
-        return Tl
-    end
+function find_liq_silicic(water::Number, co2::Number, P::Number, ini_eps_x::Number)
+    a, dadx, dady, dadz, b, dbdx, dbdy, dbdz, c, dcdx, dcdy, dcdz = parameters_melting_curve_silicic(100*water,100*co2,P)
+    f(x) = a*erfc(b*(x-c))-ini_eps_x
+    x0 = 1000 # in celsius
+    Tll = fzero(f, (0, x0), maxevals=100)
+    Tl = Tll + 273.15
+    return Tl
+end
+
+"""
+    find_liq_mafic(water::Number, co2::Number, P::Number, ini_eps_x::Number)
+
+# Arguments
+-`water`: Weight fration of the H2O in magma.
+-`co2`: Weight fration of the CO2 in magma.
+-`P`: Pressure (Pa)
+-`ini_eps_x`: The starting volumn fraction of crystal.
+"""
+function find_liq_mafic(water::Number, co2::Number, P::Number, ini_eps_x::Number)
+    a, dadx, dady, dadz, b, dbdx, dbdy, dbdz = parameters_melting_curve_mafic(100*water,100*co2,P)
+    Tl=(ini_eps_x-b)/a
+    Tl=Tl+273.15
+    return Tl
 end
 
 """
@@ -429,7 +444,7 @@ end
 """
 function crystal_fraction(composition::String, T::Number, P::Number, mH2O::Number, mCO2::Number)
     if !(composition in ["silicic", "mafic"])
-        error("composition must be \"silicic\" or \"mafic\".")
+        @error("composition must be \"silicic\" or \"mafic\".")
     else
         # NEW VERSION WITH SAGE's PARAMETERIZATION
         if composition == "silicic"
