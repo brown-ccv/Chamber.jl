0bc2836c82a00065775d5f6628f60b33c816653c
CallieHsu <kelly811113@gmail.com>
2022-10-18 14:43:26 -0400

    Fixed: #21

--- scripts/runCode.jl
+++ scripts/runCode.jl
@@ -73,11 +73,12 @@ function chamber(composition::String, end_time::Number, log_volume_km3::Number, 
         P_0 = P_0 + DP_crit
     end
 
-    if composition == "silicic"
-        T_0 = find_liq_silicic(InitialConc_H2O, InitialConc_CO2, P_0, ini_eps_x)
-    elseif composition == "mafic"
-        T_0 = find_liq_mafic(InitialConc_H2O, InitialConc_CO2, P_0, ini_eps_x)
-    end
+    T_0 = find_liq(composition, InitialConc_H2O, InitialConc_CO2, P_0, ini_eps_x)
+    # if composition == "silicic"
+    #     T_0 = find_liq_silicic(InitialConc_H2O, InitialConc_CO2, P_0, ini_eps_x)
+    # elseif composition == "mafic"
+    #     T_0 = find_liq_mafic(InitialConc_H2O, InitialConc_CO2, P_0, ini_eps_x)
+    # end
     T_in = T_0 + 50        # Temperature of inflowing magma (K)
     param["T_in"] = T_in
 
