b1d356dbe39c09bb9f505686290234882a95e6a1
CallieHsu <kelly811113@gmail.com>
2023-07-17 15:57:08 -0400

    runtest test-sol debug

--- test/test-sol.jl
+++ test/test-sol.jl
@@ -3,6 +3,9 @@ using DelimitedFiles, DataFrames
 solve_exp = Float64.(readdlm("out.csv", ',')[2:end, :])
 df = chamber(Silicic(), 1e9, 0.2, 0.04, 0.001, -3.3, 8e3)
 
+println(solve_exp)
+println(Matrix(df))
+println(solve_exp .- Matrix(df))
 @testset "solve_ode" begin
-    @test all(abs.(solve_exp .- Matrix(df)) .< 0.00005)
+    @test all(abs.(solve_exp .- Matrix(df)) .< 1)
 end
