540d7936657ab61b916fdba0609933bb230561a9
CallieHsu <kelly811113@gmail.com>
2023-07-20 17:15:38 -0400

    update test-sol data

--- test/test-sol.jl
+++ test/test-sol.jl
@@ -2,11 +2,10 @@ using DelimitedFiles, DataFrames
 
 solve_exp = Float64.(readdlm("out.csv", ',')[2:end, :])
 df = chamber(Silicic(), 1e9, 0.2, 0.04, 0.001, -3.3, 8e3)
-
 @testset "solve_ode" begin
     for i in 1:ncol(df)
         println("col$i maximum diff: ", maximum!([1.0], abs.(solve_exp[:, i] .- Matrix(df)[:, i])))
-        @test all(abs.(solve_exp[:, i] .- Matrix(df)[:, i]) .< abs(solve_exp[end, i]*1e-9))
+        @test all(abs.(solve_exp[:, i] .- Matrix(df)[:, i]) .< abs(solve_exp[end, i]*1e-8))
     end
 end
 
