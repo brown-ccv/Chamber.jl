25c417ee3a356ea532fb2ede54720e01618ed248
CallieHsu <kelly811113@gmail.com>
2023-07-24 19:27:01 -0400

    Add parallelization for array chamber

--- src/plot_figs.jl
+++ src/plot_figs.jl
@@ -1,9 +1,7 @@
 function plot_sol(df, x_axis_col, y_axis_col)
-    t = df[:, x_axis_col]
-    y = df[:, y_axis_col]
     return plot(
-        t,
-        y;
+        df[:, x_axis_col],
+        df[:, y_axis_col];
         xaxis=x_axis_col,
         yaxis=y_axis_col,
         label="Chamber.jl",
@@ -14,18 +12,11 @@ end
 
 function plot_figs(df::DataFrame, path::String)::Nothing
     ENV["GKSwstype"] = "100"  # magic environmental variable for Plots
-    p1 = plot_sol(df, "time", "P+dP")
-    p2 = plot_sol(df, "time", "T")
-    p3 = plot_sol(df, "time", "eps_g")
-    p4 = plot_sol(df, "time", "V")
-    p5 = plot_sol(df, "time", "X_CO2")
-    p6 = plot_sol(df, "time", "total_mass")
-
-    savefig(p1, "$path/1_P.png")
-    savefig(p2, "$path/2_T.png")
-    savefig(p3, "$path/3_eps_g.png")
-    savefig(p4, "$path/4_V.png")
-    savefig(p5, "$path/5_X_CO2.png")
-    savefig(p6, "$path/6_tot_Mass.png")
+    savefig(plot_sol(df, "time", "P+dP"), "$path/1_P.png")
+    savefig(plot_sol(df, "time", "T"), "$path/2_T.png")
+    savefig(plot_sol(df, "time", "eps_g"), "$path/3_eps_g.png")
+    savefig(plot_sol(df, "time", "V"), "$path/4_V.png")
+    savefig(plot_sol(df, "time", "X_CO2"), "$path/5_X_CO2.png")
+    savefig(plot_sol(df, "time", "total_mass"), "$path/6_tot_Mass.png")
     return nothing
 end
