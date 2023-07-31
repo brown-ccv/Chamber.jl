12dfdd14643cf30563c8446d8695f423d50b0d46
CallieHsu <kelly811113@gmail.com>
2023-07-31 11:29:32 -0400

    add docstring for check_for_duplicates #253

--- src/utils.jl
+++ src/utils.jl
@@ -471,6 +471,12 @@ function ic_phase_conversion(
     end
 end
 
+"""
+    check_for_duplicates(log_volume_km3_vector::Union{Float64,Vector{Float64}}, InitialConc_H2O_vector::Union{Float64,Vector{Float64}}, InitialConc_CO2_vector::Union{Float64,Vector{Float64}}, log_vfr_vector::Union{Float64,Vector{Float64}}, depth_vector::Union{Float64,Vector{Float64}})::Nothing
+
+This function checks if any of the input arrays contain duplicate elements. If duplicates are found, it raises an error indicating which arguments have duplicates.
+- This function is used within the `chamber` function
+"""
 function check_for_duplicates(
     log_volume_km3_vector::Union{Float64,Vector{Float64}},
     InitialConc_H2O_vector::Union{Float64,Vector{Float64}},
